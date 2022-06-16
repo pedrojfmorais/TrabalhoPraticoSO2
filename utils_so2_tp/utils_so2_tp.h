f21883bea1bf5bce254d2a79df46872f8b9a4f23
Tymex28 <telmex.7@gmail.com>
2022-06-17 00:42:15 +0100

    aa

--- utils_so2_tp/utils_so2_tp.h
+++ utils_so2_tp/utils_so2_tp.h
@@ -161,6 +161,7 @@ typedef struct {
 //Struct para guardar todas as threads do servidor
 #define N_THREADS_SERVIDOR 4
 typedef struct {
+
 	HANDLE hThreads[N_THREADS_SERVIDOR];
 	
 	HANDLE hEventFecharTudo;
@@ -172,13 +173,16 @@ typedef struct {
 //Struct para os namedPipe
 typedef struct {
 	HANDLE hInstancia;
+
 	OVERLAPPED overlap; //estrutura overlapped para uso asincrono
+
 	BOOL ativo; //para ver se já tem cliente ou não
 } DadosPipe;
 
 typedef struct {
 	DadosPipe hPipes[N_JOGADORES];
 	DadosJogo* jogos[N_JOGADORES];
+
 	HANDLE hEvents[N_JOGADORES];
 
 	//Evento que define quando fechar todos os programas servidor, monitor
@@ -189,7 +193,6 @@ typedef struct {
 	int deveContinuar;
 
 } PartilhaJogoServidorCliente;
-
 
 //Struct para guardar todas as threads do monitor
 #define N_THREADS_MONITOR 2
@@ -204,8 +207,11 @@ typedef struct {
 
 typedef struct {
 	DadosPipe hPipes[N_JOGADORES];
+	
 	HANDLE hEvents[N_JOGADORES];
+
 	HANDLE hMutex;
+
 	int* deveContinuar;
 } ThreadsCliente;
 
